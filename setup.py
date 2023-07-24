88957f19af20663ee164a38ff09387c958cc04c4
RSmolak <rafal.smolak@gmail.com>
2023-07-24 20:59:20 +0200

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function returns the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        [requirement.replace('\n', '') for requirement in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+    name='MLOps_project',
+    version='0.0.1',
+    author='Rafal Smolak',
+    author_email='rafalsmolak00@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
